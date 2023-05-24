280b21e9766edc80ed7730a31a82dcc41bf2b429
ming <fox20431@gmail.com>
2023-05-24 20:25:29 +0800

    Correct the github actions workflow

--- src/main/java/com/hihusky/omoidasu/config/CustomWebMvcConfigurer.java
+++ src/main/java/com/hihusky/omoidasu/config/CustomWebMvcConfigurer.java
@@ -1,11 +1,17 @@
 package com.hihusky.omoidasu.config;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Component
 public class CustomWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods("*").maxAge(3600);
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
     }
 }
